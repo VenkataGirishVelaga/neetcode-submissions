e28bef05a357dfc4b7d21f4ca995e0a1a9185b23
VELAGA VENKATA GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2026-05-02 17:07:08 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans = "";
+
+        for(char ch : s){
+            if(isalnum(ch)){
+                ans += tolower(ch);
+            }
+        }
+
+        int i = 0, j = ans.size() - 1;
+        while(i < j){
+            if(ans[i] != ans[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
