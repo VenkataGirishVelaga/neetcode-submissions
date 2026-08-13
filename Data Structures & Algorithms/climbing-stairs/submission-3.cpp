88dc58ad92a6eb53700e790d18dc75a8be9435a7
VELAGA VENKATA GIRISH <151516223+VenkataGirishVelaga@users.noreply.github.com>
2026-08-13 17:09:33 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 1) return 1;
+        int p1 = 1;
+        int p2 = 1;
+        int c = 0;
+        for(int i = 2; i <= n; i++){
+            c = p1 + p2;
+            p1 = p2;
+            p2 = c;
+        }
+        return c;
+    }
+};
